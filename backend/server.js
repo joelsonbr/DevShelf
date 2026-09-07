d2bfed0302e087054a9c9c38f5797bebd2961335
Joelson <joelsonpaz1@gmail.com>
2026-09-07 00:50:44 -0300

    Atualiazação: Resolvendo problemas.

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express();
 const PORT = process.env.PORT ||3000;
 
-app.use(express.static(path.join(__dirname, "frontend")));
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get('/', (req, res) => {
     console.log('Alguém acessou o DevShelf!');
